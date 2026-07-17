3125aa65d848f72b4e38de133dd5a1e80d6c3a95
Kewsito <ksalinas191@gmail.com>
2026-07-17 01:31:56 -0300

    delete: Delete console.logs

--- public/anime.js
+++ public/anime.js
@@ -1,10 +1,5 @@
 import { animate } from 'https://cdn.jsdelivr.net/npm/animejs@4.5.0/dist/modules/index.js';
 
-console.log("Procesando animacion");
-			
-// 1. El objetivo ('#img-perfil') va como PRIMER argumento.
-
-// 2. Las opciones van en un objeto como SEGUNDO argumento.
 			animate('#img-perfil', {
 					x: '10cqw',
 					duration: 1300,
